472f88f2675000477e6d799d9534d0c8361b37a5
Sankalp <sink257@hotmail.com>
2021-01-26 17:02:05 +0800

    Initial Commit

---
+++ Team8_ADProject/src/main/java/sg/edu/iss/team8adproject/Team8AdProjectApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.iss.team8adproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team8AdProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team8AdProjectApplication.class, args);
+	}
+
+}
